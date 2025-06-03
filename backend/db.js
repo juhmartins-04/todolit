234e46d64143287b59333459cb384cb851f73fd4
juhmartins-04 <juuhhzinha2000@gmail.com>
2025-06-03 10:50:38 -0300

    correcao

--- backend/db.js
+++ backend/db.js
@@ -2,11 +2,12 @@ const mysql = require('mysql2');
 require('dotenv').config();
 
 const db = mysql.createConnection({
-  host: 'localhost',
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
-  database: 'todolist'
+  database: process.env.DB_NAME
 });
+
 
 db.connect(err => {
   if (err) throw err;
